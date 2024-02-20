7139f2ebcade10651fa7429b154b91637eae7a80
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2024-02-20 16:30:59 -0600

    Initial Commit

---
+++ Appboard/AppboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppboardApp.swift
+//  Appboard
+//
+//  Created by SM on 2024.02.20.
+//
+
+import SwiftUI
+
+@main
+struct AppboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appboard
+//
+//  Created by SM on 2024.02.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
