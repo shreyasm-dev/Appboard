3a74ff77b913217bce30a9d00f39149b259a3adb
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2024-02-20 16:31:23 -0600

    Formatting

--- Appboard/AppboardApp.swift
+++ Appboard/AppboardApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct AppboardApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
